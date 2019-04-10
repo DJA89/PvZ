b9a3ba29e2ebf6ef8d8ccb951d770e7f4ad0dfbb
fabioupc <44349572+fabioupc@users.noreply.github.com>
2019-04-10 17:36:29 +0100

    spawnpoint inside plant1 prefab

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -25,11 +25,13 @@ public class Shoot : MonoBehaviour
         if (timeToShoot <= 0.0f)
         {
             timeToShoot = 0.5f;
-            GameObject obj = (GameObject)Instantiate(shot, shotSpawn.transform.position, transform.rotation);
-            Vector3 toLeftBorder = new Vector3(0.0f, 0.0f, obj.GetComponent<Collider>().bounds.size.z);
+            // as the shots are spawned as CHILDREN of the Plant, they automatically inherit the plant.transform
+            GameObject obj = (GameObject)Instantiate(shot, shotSpawn.transform.position, shotSpawn.transform.rotation); // just apply spawnPoint transform 
+            // move by half x size (move out of plant to the front)
+            Vector3 toLeftBorder = new Vector3(obj.GetComponent<Collider>().bounds.size.x/2, 0.0f, 0.0f);
             obj.transform.Translate(toLeftBorder);
             obj.transform.parent = transform; // add as child to current
-            obj.GetComponent<Rigidbody>().AddForce(1000.0f, 0.0f, 0.0f);
+            obj.GetComponent<Rigidbody>().AddForce(1000.0f, 0.0f, 0.0f); // shoot
             //AudioSource.PlayClipAtPoint(sound, transform.position);
         }
 
