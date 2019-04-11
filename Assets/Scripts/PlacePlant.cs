4df261ee1a743cf3b727a9c6254b05548ea828f5
fabioupc <44349572+fabioupc@users.noreply.github.com>
2019-04-11 01:05:41 +0100

    generate collider grid on ground

---
+++ Assets/Scripts/PlacePlant.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacePlant : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // left mouse button
+        {
+            Debug.Log("Pressed primary button.");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.transform.gameObject)
+                print("HIT");
+            }
+        }
+    }
+}
