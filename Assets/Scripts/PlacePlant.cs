eeb04bdd4bca82ee7b1b4e2ef11a3ccae5e10b8b
fabioupc <44349572+fabioupc@users.noreply.github.com>
2019-04-11 16:10:39 +0200

    shadow on hover and spawn on click

--- Assets/Scripts/PlacePlant.cs
+++ Assets/Scripts/PlacePlant.cs
@@ -6,26 +6,68 @@ using UnityEngine.EventSystems;
 public class PlacePlant : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public GameObject plant1Template;
-
-    private GameObject currentPlant;
-
+    
+    private GameObject plantShadow;
     private Renderer cellRenderer;
     private Color cellColor;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        cellRenderer = gameObject.GetComponent<Renderer>();
+        cellRenderer.material.color = Color.blue;
+
+        // no children => cell free
+        if (transform.childCount == 0) // no children => cell free
+        {
+            // show shadow of plant
+            plantShadow = spawnPlant(); // spawn
+            SetRendererAlphas(0.5f, GetComponentsInChildren<Renderer>()); // make semi-transparent
+            plantShadow.GetComponent<Shoot>().enabled = false; // don't shoot
+        }
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         cellRenderer = gameObject.GetComponent<Renderer>();
         cellRenderer.material.color = Color.red;
 
-        // if has no plant already there...
-        if (transform.childCount == 0) // no children => cell free
+        // only shadow present?
+        if (transform.childCount == 1 && transform.GetChild(0).gameObject == plantShadow)
         {
-            // ...spawn a plant!
+            // remove shadow
+            Destroy(plantShadow);
+            plantShadow = null;
+            // spawn real plant
             GameObject newPlant = spawnPlant();
         }
     }
 
-    private GameObject spawnPlant()
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // if we have a shadow
+        if (plantShadow != null)
+        {
+            // remove shadow
+            Destroy(plantShadow);
+            plantShadow = null;
+        }
+    }
+
+    private void SetRendererAlphas(float alpha, Renderer[] mRenderers)
+    {
+        // from https://answers.unity.com/questions/1031416/make-a-complex-object-semi-transparent-without-cha.html
+        for (int i = 0; i < mRenderers.Length; i++)
+        {
+            for (int j = 0; j < mRenderers[i].materials.Length; j++)
+            {
+                Color matColor = mRenderers[i].materials[j].color;
+                matColor.a = alpha;
+                mRenderers[i].materials[j].color = matColor;
+            }
+        }
+    }
+
+private GameObject spawnPlant()
     {
         Vector3 plantSpawnPoint = new Vector3(0.0f, 0.0f, -0.75f); // relative to (parent) cell
         GameObject newPlant = (GameObject)Instantiate(plant1Template, plantSpawnPoint + transform.position, transform.rotation, transform);
@@ -36,19 +78,6 @@ public class PlacePlant : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         newPlantScale.z /= transform.lossyScale.z;
         newPlant.transform.localScale = newPlantScale;
         return newPlant;
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        cellRenderer = gameObject.GetComponent<Renderer>();
-        cellRenderer.material.color = Color.blue;
-
-
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-
     }
 
     // Start is called before the first frame update
