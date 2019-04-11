c1b71c5a679b8c09b941c2377cbe5b5fe1612e77
fabioupc <44349572+fabioupc@users.noreply.github.com>
2019-04-11 14:55:08 +0200

    removed old Ground prefab

--- Assets/Scripts/PlacePlant.cs
+++ Assets/Scripts/PlacePlant.cs
@@ -21,27 +21,34 @@ public class PlacePlant : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         if (transform.childCount == 0) // no children => cell free
         {
             // ...spawn a plant!
-            Vector3 plantSpawnPoint = new Vector3(0.0f, 0.0f, -0.75f); // relative to (parent) cell
-            GameObject newPlant = (GameObject)Instantiate(plant1Template, plantSpawnPoint + transform.position, transform.rotation, transform);
-            // divide by absolute (lossy) cell scale (otherwise plant gets squashed)
-            Vector3 newPlantScale = newPlant.transform.localScale;
-            newPlantScale.x /= transform.lossyScale.x;
-            newPlantScale.y /= transform.lossyScale.y;
-            newPlantScale.z /= transform.lossyScale.z;
-            newPlant.transform.localScale = newPlantScale;
+            GameObject newPlant = spawnPlant();
         }
+    }
+
+    private GameObject spawnPlant()
+    {
+        Vector3 plantSpawnPoint = new Vector3(0.0f, 0.0f, -0.75f); // relative to (parent) cell
+        GameObject newPlant = (GameObject)Instantiate(plant1Template, plantSpawnPoint + transform.position, transform.rotation, transform);
+        // divide by absolute (lossy) cell scale (otherwise plant gets squashed)
+        Vector3 newPlantScale = newPlant.transform.localScale;
+        newPlantScale.x /= transform.lossyScale.x;
+        newPlantScale.y /= transform.lossyScale.y;
+        newPlantScale.z /= transform.lossyScale.z;
+        newPlant.transform.localScale = newPlantScale;
+        return newPlant;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         cellRenderer = gameObject.GetComponent<Renderer>();
         cellRenderer.material.color = Color.blue;
+
+
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        cellRenderer = gameObject.GetComponent<Renderer>();
-        //cellRenderer.material.color = Color.black;
+
     }
 
     // Start is called before the first frame update
