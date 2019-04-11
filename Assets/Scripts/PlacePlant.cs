664f184cf51ca21e0114755b446b249aa5ae0ab1
fabioupc <44349572+fabioupc@users.noreply.github.com>
2019-04-11 14:09:20 +0200

    click spawns plant on cell

--- Assets/Scripts/PlacePlant.cs
+++ Assets/Scripts/PlacePlant.cs
@@ -5,7 +5,8 @@ using UnityEngine.EventSystems;
 
 public class PlacePlant : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
-    //public GameObject CellContainer;
+    public GameObject plant1Template;
+
     private Renderer cellRenderer;
 
     private Color cellColor;
@@ -14,6 +15,20 @@ public class PlacePlant : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     {
         cellRenderer = gameObject.GetComponent<Renderer>();
         cellRenderer.material.color = Color.red;
+
+        // if has no plant already spawn a plant
+        //if (KeyNotFoundException plant)
+        // as the shots are spawned as CHILDREN of the Plant, they automatically inherit the plant.transform
+        Vector3 plantSpawn = new Vector3(0.0f, 0.0f, -0.75f);
+        GameObject newPlant = (GameObject)Instantiate(plant1Template, plantSpawn + transform.position, transform.rotation, transform);
+        //objToSpawn = new GameObject("Cool GameObject made from Code");
+        // divide by absolute cell scale (otherwise plant gets squashed)
+        Vector3 newPlantScale = newPlant.transform.localScale;
+        newPlantScale.x /= transform.lossyScale.x;
+        newPlantScale.y /= transform.lossyScale.y;
+        newPlantScale.z /= transform.lossyScale.z;
+        newPlant.transform.localScale = newPlantScale;
+        //print(newPlant.transform.position.x);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
