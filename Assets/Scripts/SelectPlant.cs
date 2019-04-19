63989ca48b99c7b8f6bdf49b9c08943b94c21fcb
fabioupc <44349572+fabioupc@users.noreply.github.com>
2019-04-19 14:50:21 +0200

    select and place works (selection invisible)

---
+++ Assets/Scripts/SelectPlant.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectPlant : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, ISelectHandler, IDeselectHandler
+{
+    public GameObject plant1Template;
+
+    private GameObject plantDragged;
+    private Transform m_DraggingPlane;
+
+    private Renderer cellRenderer;
+    private Color cellColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // show tooltip
+
+
+        //cellRenderer = gameObject.GetComponent<Renderer>();
+        //cellRenderer.material.color = Color.blue;
+
+        //// no children => cell free
+        //if (transform.childCount == 0) // no children => cell free
+        //{
+        //    float transparency = 0.5f;
+        //    // show shadow of plant
+        //    plantShadow = spawnPlant(); // spawn
+        //    SetRendererAlphas(transparency, GetComponentsInChildren<Renderer>()); // make semi-transparent
+        //    plantShadow.GetComponent<Shoot>().enabled = false; // don't shoot
+        //}
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // select clicked object (current object)
+        SelectionManager.Instance.Selected = plant1Template;
+
+        //cellRenderer = gameObject.GetComponent<Renderer>();
+        //cellRenderer.material.color = Color.red;
+
+        //// only shadow present?
+        //if (transform.childCount == 1 && transform.GetChild(0).gameObject == plantShadow)
+        //{
+        //    // remove shadow
+        //    Destroy(plantShadow);
+        //    plantShadow = null;
+        //    // spawn real plant
+        //    GameObject newPlant = spawnPlant();
+        //}
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // remove tooltip
+
+
+        //// if we have a shadow
+        //if (plantShadow != null)
+        //{
+        //    // remove shadow
+        //    Destroy(plantShadow);
+        //    plantShadow = null;
+        //}
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        //plantDragged = spawnPlant(); // spawn
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        //if (plantDragged != null)
+        //{
+        //    plantDragged.GetComponent<Shoot>().enabled = false; // don't shoot
+        //    //// remove shadow
+        //    //Destroy(plantDragged);
+        //    //plantDragged = null;
+        //}
+    }
+
+    private void SetRendererAlphas(float alpha, Renderer[] mRenderers)
+    {
+        // from https://answers.unity.com/questions/1031416/make-a-complex-object-semi-transparent-without-cha.html
+        for (int i = 0; i < mRenderers.Length; i++)
+        {
+            for (int j = 0; j < mRenderers[i].materials.Length; j++)
+            {
+                Color matColor = mRenderers[i].materials[j].color;
+                matColor.a = alpha;
+                mRenderers[i].materials[j].color = matColor;
+            }
+        }
+    }
+
+    private GameObject spawnPlant()
+    {
+        Vector3 plantSpawnPoint = new Vector3(0.0f, 0.0f, -0.75f); // relative to (parent) cell
+        GameObject newPlant = (GameObject)Instantiate(plant1Template, plantSpawnPoint + transform.position, transform.rotation, transform);
+        // divide by absolute (lossy) cell scale (otherwise plant gets squashed)
+        Vector3 newPlantScale = newPlant.transform.localScale;
+        newPlantScale.x /= transform.lossyScale.x;
+        newPlantScale.y /= transform.lossyScale.y;
+        newPlantScale.z /= transform.lossyScale.z;
+        newPlant.transform.localScale = newPlantScale;
+        return newPlant;
+    }
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // ungrey source tile, stop moving plant, place plant
+        Debug.Log("OnEndDrag");
+
+        //cellRenderer = gameObject.GetComponent<Renderer>();
+        //cellRenderer.material.color = Color.red;
+
+        //// if we have a dragged object
+        //if (plantDragged != null)
+        //{
+        //    // remove shadow
+        //    Destroy(plantDragged);
+        //    plantDragged = null;
+        //}
+    }
+
+    private void SetDraggedPosition(PointerEventData data)
+    {
+        //// adapted from https://docs.unity3d.com/ScriptReference/EventSystems.IDragHandler.html
+        //if (data.pointerEnter != null && data.pointerEnter.transform != null)
+        //    m_DraggingPlane = data.pointerEnter.transform;
+
+        //var rt = plantDragged.transform;
+        //Vector3 globalMousePos;
+        //if (RectTransformUtility.ScreenPointToWorldPointInRectangle(m_DraggingPlane, data.position, data.pressEventCamera, out globalMousePos))
+        //{
+
+        //    rt.position = globalMousePos;
+        //    rt.rotation = m_DraggingPlane.rotation;
+        //}
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // grey source tile, start moving plant
+        Debug.Log("OnBeginDrag");
+
+        //cellRenderer = gameObject.GetComponent<Renderer>();
+        //cellRenderer.material.color = Color.blue;
+
+        //// create dragged copy (no shooting)
+        //GameObject newPlant = spawnPlant();
+        //plantDragged.GetComponent<Shoot>().enabled = false; // don't shoot
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // move plant
+        Debug.Log("OnDrag");
+
+        //// if we have a shadow
+        //if (plantDragged != null)
+        //{
+        //    SetDraggedPosition(eventData);
+        //}
+    }
+
+}
---
+++ Assets/Scripts/SetupPlantToolbar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupPlantToolbar : MonoBehaviour
+{
+    public GameObject toolbarPlantTemplate;
+    public GameObject plantsParent;
+
+    int numberSlotsMaximum = 9;
+    int numberSlotsStarting = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 planterSize = gameObject.GetComponent<Collider>().bounds.size;
+        Vector3 toParentBottomLeft = new Vector3(-planterSize.x / 2, planterSize.y / 2, -planterSize.z / 2);
+        Vector3 cellSize = new Vector3(planterSize.x / numberSlotsMaximum, 0.0f, planterSize.z);
+        Vector3 toCellCenter = new Vector3(cellSize.x / 2, 0.0f, cellSize.z / 2);
+        for (int i = 0; i < numberSlotsStarting; i++) // only create numberSlotsStarting slots
+        {
+            Vector3 cellPositionInGrid = new Vector3(i * cellSize.x, 0.0f, 0.0f);
+            // as cells are spawned as Grand-CHILDREN of the Ground, so they automatically inherit transform from Ground
+            GameObject newCell = (GameObject)Instantiate(toolbarPlantTemplate, transform.position + toParentBottomLeft + cellPositionInGrid + toCellCenter, plantsParent.transform.rotation, plantsParent.transform);
+            newCell.transform.localScale = new Vector3(1.0f / numberSlotsMaximum, 1.0f, 1.0f);
+
+            // unity sometimes doesn't set position, so set localPosition (https://answers.unity.com/questions/225729/gameobject-positionset-not-working.html)
+            // also yOffset = 0.001 to prevent z-fighting with ground
+            newCell.transform.localPosition = new Vector3(newCell.transform.localPosition.x, 0.001f, newCell.transform.localPosition.z);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
