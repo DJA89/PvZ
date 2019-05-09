f01bf2b460ee1377944782f8b6eb27e2d872fdc0
fabioupc <44349572+fabioupc@users.noreply.github.com>
2019-05-09 18:33:57 +0200

    toolbar has plant prices

--- Assets/Scripts/SetupPlantToolbar.cs
+++ Assets/Scripts/SetupPlantToolbar.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class SetupPlantToolbar : MonoBehaviour
 {
-    public GameObject toolbarObject;
     public TextAsset plantsConfig;
+    public GameObject toolbarObject;
     public GameObject toolbarPlantTemplates;
-    public GameObject plantsList;
+    public GameObject priceTagTemplates;
+    public GameObject plantList;
 
     int numberSlotsMaximum = 8;
 
@@ -28,32 +29,32 @@ public class SetupPlantToolbar : MonoBehaviour
         // 1 slot for sun + all plants
         Vector3 cellSize = new Vector3(toolbarSize.x / (numberSlotsMaximum + 1), 0.0f, toolbarSize.z);
         Vector3 toCellCenter = new Vector3(cellSize.x / 2, 0.0f, cellSize.z / 2);
-
-        //// scale plantlist so plants templates don't get huge
-        //Vector3 newPlantlistScale = plantsList.transform.localScale;
-        //newPlantlistScale.x /= plantsList.transform.lossyScale.x;
-        //newPlantlistScale.y /= plantsList.transform.lossyScale.y;
-        //newPlantlistScale.z /= plantsList.transform.lossyScale.z;
-        //plantsList.transform.localScale = newPlantlistScale;
-
+        
         int plantsInToolbarCount = toolbarPlantTemplates.transform.childCount;
         for (int i = 0; i < plantsInToolbarCount; i++) // only create numberSlotsStarting slots
         {
+            // get new cell
             Vector3 cellPositionInGrid = new Vector3((i + 1) * cellSize.x, 0.0f, 0.0f); // i+1 because of sun slot
-            // as cells are spawned as Grand-CHILDREN of the Ground, so they automatically inherit transform from Ground
+
+            // create new plant
             GameObject newToolbarPlant = toolbarPlantTemplates.transform.GetChild(i).gameObject;
             Vector3 newToolbarPlantPosition = transform.position + toParentFrontLeft + cellPositionInGrid + toCellCenter;
-            GameObject newPlant = (GameObject)Instantiate(newToolbarPlant, newToolbarPlantPosition, plantsList.transform.rotation, plantsList.transform);
+            GameObject newPlant = (GameObject)Instantiate(newToolbarPlant, newToolbarPlantPosition, plantList.transform.rotation, plantList.transform);
             // if has shoot script => disable it
             if (newPlant.GetComponent<Shoot>() != null)
             {
                 newPlant.GetComponent<Shoot>().enabled = false;
             }
             newPlant.AddComponent<SelectPlant>();
-
             // unity sometimes doesn't set position, so set localPosition (https://answers.unity.com/questions/225729/gameobject-positionset-not-working.html)
             // also yOffset = 0.001 to prevent z-fighting with ground
             newPlant.transform.localPosition = new Vector3(newPlant.transform.localPosition.x, newPlant.transform.localPosition.y + 0.001f, newPlant.transform.localPosition.z);
+
+            // create new price tag
+            GameObject priceTagTemplate = priceTagTemplates.transform.GetChild(0).gameObject;
+            float priceTagPositionX = (transform.position + toParentFrontLeft + cellPositionInGrid + toCellCenter).x - 0.8f;
+            Vector3 priceTagPosition = priceTagTemplate.transform.position + new Vector3(priceTagPositionX, 0, 0);
+            GameObject newPriceTag = (GameObject)Instantiate(priceTagTemplate, priceTagPosition, plantList.transform.rotation, plantList.transform);
         }
     }
 
