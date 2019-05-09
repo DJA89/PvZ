51a00eae056965bb8325f0beb29502bde827b4d0
fabioupc <44349572+fabioupc@users.noreply.github.com>
2019-05-09 17:02:31 +0200

    collect suns to get sun score

---
+++ Assets/Scripts/CollectSuns.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CollectSuns : MonoBehaviour, IPointerClickHandler
+{
+    public static readonly int SUN_VALUE = 50;
+
+    // this sun clicked
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("sun clicked");
+        // add sun value to sun score
+        Globals.Instance.SunScore += SUN_VALUE;
+        // remove sun
+        Destroy(gameObject);
+    }
+}
