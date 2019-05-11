d42f7a37940d97a6b901a1e922210a5690e5e7c6
fabioupc <44349572+fabioupc@users.noreply.github.com>
2019-05-11 21:54:16 +0200

    start game from any scene
    just attach the DevPreloader script the camera (actually any objects, but like this it stays organized)

---
+++ Assets/Scripts/Loading&Globals/DevPreload.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevPreload : MonoBehaviour
+{
+    // this should run absolutely first; use script-execution-order to do so.
+    // (of course, normally never use the script-execution-order feature,
+    // this is an unusual case, just for development.)
+    void Awake()
+    {
+        GameObject check = GameObject.Find("__app");
+        if (check == null) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("_preload");
+        }
+    }
+}
