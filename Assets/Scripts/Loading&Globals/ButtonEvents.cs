996360f48ee5eed58fa203287cb53580f88285c5
fabioupc <44349572+fabioupc@users.noreply.github.com>
2019-05-14 20:43:17 +0200

    button hover sound

---
+++ Assets/Scripts/Loading&Globals/ButtonEvents.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonEvents : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioSource sound;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (gameObject.GetComponent<Button>().IsInteractable())
+        {
+            sound.Play();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
