8cc780a2d7c5d2b4c570a8ce6093ab38680d0a0c
fabioupc <44349572+fabioupc@users.noreply.github.com>
2019-05-17 18:42:26 +0200

    calibrated volume of shooting/hitting sfx

--- Assets/Scripts/Plants/Hit.cs
+++ Assets/Scripts/Plants/Hit.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Hit : MonoBehaviour
 {
     public AudioClip sound;
+    float RELATIVE_SFX_VOLUME = 0.1f;
 
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject, 0.5F);
-        AudioSource.PlayClipAtPoint(sound, Camera.main.transform.position, Globals.Instance.sfxVolume);
+        AudioSource.PlayClipAtPoint(sound, Camera.main.transform.position, Globals.Instance.sfxVolume * RELATIVE_SFX_VOLUME);
     }
 }
