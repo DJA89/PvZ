b7e65a46ae3a39e3c6e572f08d0bef107d48dd00
DJA <drod89@gmail.com>
2019-05-24 11:34:22 +0200

    Make bullets dissappear if they go off screen

--- Assets/Scripts/OffScreen.cs
+++ Assets/Scripts/OffScreen.cs
@@ -4,17 +4,15 @@ using System.Collections;
 public class OffScreen : MonoBehaviour
 {
 
-    private int offScreenCoordinate;
 
     private void Start()
     {
-        offScreenCoordinate = 17;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x > 17)
+        if (transform.position.x > 17 || transform.position.x < -17)
         {
             Destroy(gameObject, 0F);
         }
